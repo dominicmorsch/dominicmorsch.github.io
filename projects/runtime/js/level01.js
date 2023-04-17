a6e604d459377f1d59bfc227542ce62e4a90cbd9
dominicmorsch <dmorsch999@gmail.com>
2023-04-17 18:04:34 +0000

    push my runtime into portfolio

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,18 +19,110 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "enemy", "x": 500, "y": groundY},
+                { "type": "reward", "x": 300, "y": groundY},
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+       /* var hitZoneSize = 25;
+        var damageFromObstacle = 10;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+        sawBladeHitZone.x = 400;
+        sawBladeHitZone.y = 100;
+        game.addGameItem(sawBladeHitZone);
+        var obstacleImage = draw.bitmap("img/sawblade.png");
+        sawBladeHitZone.addChild(obstacleImage);*/
 
-        
-        
-        
+
+        //creating saw blade function
+        function createSawBlade(x , y){
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);
+            //picture of 8 bit sawblade 
+            var obstacleImage = draw.bitmap("img/sawblade.png");
+            sawBladeHitZone.addChild(obstacleImage);
+           obstacleImage.x = -25;
+           obstacleImage.y = -25;
+
+           obstacleImage.scaleX = .25;
+           obstacleImage.scaleY = .25;
+        }
+        //creating enemy function 
+        function create(){}
+        function createEnemy(x,y){
+            var enemy = game.createGameItem("enemy", 25);
+            //picture of 8 bit ghost 
+            var ghost = draw.bitmap("img/ghost.png");
+            //speed of the enemy
+            ghost.x = -25;
+            ghost.y = -25;
+            //size of the enemy
+            ghost.scaleX = .1;
+            ghost.scaleY = .1;
+            enemy.addChild(ghost);
+            enemy.x = x;
+            enemy.y = y;
+            game.addGameItem(enemy);  
+            enemy.velocityX = -1;  
+            enemy.onPlayerCollision = function (){
+                game.changeIntegrity(-10);
+            };
+            enemy.onProjectileCollision = function(){
+                game.increaseScore(100);
+                enemy.fadeOut();
+            }
+        }
+        //creating reawrd function 
+        function createReward(x, y){
+            var reward = game.createGameItem("reward", 25);
+            //picture of 8 bit coin
+            var coin = draw.bitmap("img/coin png.png");
+            //size of the reward
+            coin.scaleX = .2;
+            coin.scaleY = .2;
+            //speed of the reward
+            coin.x = -25;
+            coin.y = -25;
+            reward.addChild(coin);
+            reward.x = x;
+            reward.y = y;
+            game.addGameItem(reward);  
+            reward.velocityX = -1; 
+            //health when collecting the reward 
+            reward.onPlayerCollision = function (){
+                game.increaseScore(100);
+                reward.fadeOut();
+            };
+            reward.onProjectileCollision = function(){
+                
+                reward.fadeOut();
+            }
+        }
+        //createEnemy(400, groundY - 50);
+        //createSawBlade(800, groundY);
+
+        //loop for level data and all game items 
+        for (var i = 0; i < levelData.gameItems.length; i++){
+                var gameItem = levelData.gameItems[i];
+                if (gameItem.type === "reward"){
+                    createReward(gameItem.x, gameItem.y);
+                }
+                if (gameItem.type === "sawblade"){
+                    createSawBlade(gameItem.x, gameItem.y);
+                }
+                if(gameItem.type === "enemy"){
+                    createEnemy(gameItem.x, gameItem.y);
+                }
+        }
         // DO NOT EDIT CODE BELOW HERE
     }
 };
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,10 +1,11 @@
 var background = function (window) {
     'use strict';
     
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
@@ -16,18 +17,19 @@ var background = function (window) {
         if(!ground || typeof(ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree = 0;
+        var buildings = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,21 +38,49 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY,'yellow');
             background.addChild(backgroundFill);
-            
+
             // TODO: 3 - Add a moon and starfield
-            
-            
+
+            for(var i = 0; i < 100; i++){
+                var circle = draw.circle(10, "white", "LightGray", 2);
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+      
+            }
+            //moon picture
+            var moon = draw.bitmap("img/moon.png");
+            //moon position
+            moon.x = canvasWidth - 300;
+            moon.y = groundY - 400;
+            //moon size
+            moon.scaleX = 0.5;
+            moon.scaleY = 0.5;
+            background.addChild(moon);
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+           //loop of buildings added
+            for (var i = 0; i < 5; ++i) {
+                var buildingHeight = 300;
+                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
+                //building size
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
+
             // TODO 4: Part 1 - Add a tree
-            
-            
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 0;
+            tree.y = groundY - 235;
+            background.addChild(tree);
+
         } // end of render function - DO NOT DELETE
-        
-        
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -58,26 +88,33 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
-            
-            
+            tree.x -= 1;
+             if (tree.x < -200) {
+            tree.x = canvasWidth;
+
+            }
+
             // TODO 5: Part 2 - Parallax
-            
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i];
+                building.x -= 0.8;
+            }
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
